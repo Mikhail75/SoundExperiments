c21e2b4d76c843e838c0a0b1c09e9135dfaaa1b3
Mikhail <glizerin@gmail.com>
2020-11-23 23:38:35 +0300

    [SimpleToneGenerator] Форматирование

--- SimpleToneGenerator/SimpleToneGenerator.cpp
+++ SimpleToneGenerator/SimpleToneGenerator.cpp
@@ -14,7 +14,7 @@ int main()
 
 	const unsigned samplingRate = 44100;	// Частота дискретизации (Hz)
 	const float toneFrequency = 440;		// Частота тона (Hz)
-	const float	amplitude = 1;				// Амплитуда (громкость) звуковой волны (0..1)
+	const float amplitude = 1;				// Амплитуда (громкость) звуковой волны (0..1)
 	const unsigned sampleDuration = 2;		// Длительность звучания семпла (сек)
 
 	const float stepChangeInRadians = 2 * PI * toneFrequency / samplingRate;
